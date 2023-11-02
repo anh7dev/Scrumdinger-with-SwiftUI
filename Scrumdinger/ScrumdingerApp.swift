cf2b45824bb8e570c9e17877abd864343e1ac58c
anh7dev <nccuong.developer@gmail.com>
2023-11-02 23:44:53 +0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Nguyễn Chí Cường on 02/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
